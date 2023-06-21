187a18bc139b0f6d77a39b17669a385cec570d11
1245012809 <1245012809@qq.com>
2023-06-21 23:50:08 +0800

    代码新增--增加了一个JsonData工具类，用于成功和失败返回状态码

---
+++ src/main/java/net/xdclass/online_xdclass/utils/JsonData.java
@@ -0,0 +1,60 @@
+package net.xdclass.online_xdclass.utils;
+
+public class JsonData {
+
+    private Integer code;
+
+    private Object data;
+
+    private String msg;
+
+    /**
+     *
+     * @param code 状态码，0成功，1处理中，-1失败
+     * @param data 业务数据
+     * @param msg 信息表示
+     */
+    public JsonData(Integer code, Object data, String msg) {
+        this.code = code;
+        this.data = data;
+        this.msg = msg;
+    }
+
+    /**
+     * 成功，不返回数据
+     * @return
+     */
+    public static JsonData buildSuccess() {
+        return new JsonData(0, null, null);
+    }
+
+    /**
+     * 成功，返回数据
+     * @param data
+     * @return
+     */
+    public static JsonData buildSuccess(Object data) {
+        return new JsonData(0, null, null);
+    }
+
+    /**
+     * 失败，固定状态码
+     * @param msg
+     * @return
+     */
+    public static JsonData buildError(String msg){
+
+        return new JsonData(-1, null, null);
+    }
+
+    /**
+     * 失败，自定义错误码和信息
+     * @param code
+     * @param msg
+     * @return
+     */
+    public static JsonData buildError(Integer code, String msg){
+
+        return new JsonData(-1, null,  null);
+    }
+}
