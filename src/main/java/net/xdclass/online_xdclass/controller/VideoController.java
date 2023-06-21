146ede9c132b91158e25ffc9b1c74bca0e04538d
1245012809 <1245012809@qq.com>
2023-06-21 23:58:58 +0800

    代码修改--简单修改了一下VideoController层的测试接口

--- src/main/java/net/xdclass/online_xdclass/controller/VideoController.java
+++ src/main/java/net/xdclass/online_xdclass/controller/VideoController.java
@@ -1,9 +1,13 @@
 package net.xdclass.online_xdclass.controller;
 
+import net.xdclass.online_xdclass.domain.Video;
 import net.xdclass.online_xdclass.service.VideoService;
+import net.xdclass.online_xdclass.utils.JsonData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/pub/video")
@@ -15,6 +19,7 @@ public class VideoController {
     @RequestMapping("list")
     public Object listVideo(){
 
-        return videoService.listVideo();
+        List<Video> videoList = videoService.listVideo();
+        return JsonData.buildSuccess(videoList);
     }
 }
