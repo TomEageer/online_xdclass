6d6541daebb17a1690a18f0defce08ccebdaffd3
1245012809 <1245012809@qq.com>
2023-06-21 23:59:33 +0800

    代码修改--忘记get和set了

--- src/main/java/net/xdclass/online_xdclass/utils/JsonData.java
+++ src/main/java/net/xdclass/online_xdclass/utils/JsonData.java
@@ -57,4 +57,28 @@ public class JsonData {
 
         return new JsonData(-1, null,  null);
     }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
 }
