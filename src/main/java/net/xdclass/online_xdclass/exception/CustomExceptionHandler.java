47e48bd1037870e36381293fe95afa91c3a95254
1245012809 <1245012809@qq.com>
2023-06-22 14:50:42 +0800

    功能新增--新增加了自定义异常的功能

---
+++ src/main/java/net/xdclass/online_xdclass/exception/CustomExceptionHandler.java
@@ -0,0 +1,34 @@
+package net.xdclass.online_xdclass.exception;
+
+import net.xdclass.online_xdclass.utils.JsonData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 异常处理类
+ */
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+
+    private final static Logger logger = LoggerFactory.getLogger(CustomExceptionHandler.class);
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public JsonData handle(Exception e){
+
+        logger.error("[ 系统异常 ]{}", e);
+        if (e instanceof XDException){
+
+            XDException xdException = (XDException) e;
+
+            return JsonData.buildError(xdException.getCode(), xdException.getMsg());
+
+        }else {
+
+            return JsonData.buildSuccess("全局异常，未知错误");
+        }
+    }
+}
---
+++ src/main/java/net/xdclass/online_xdclass/exception/XDException.java
@@ -0,0 +1,33 @@
+package net.xdclass.online_xdclass.exception;
+
+/**
+ * 自定义异常
+ */
+public class XDException extends RuntimeException {
+
+    private Integer code;
+
+    private String msg;
+
+    private XDException(Integer code, String msg){
+
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
