262e853e275c137784a0915534fcb7aa3cff4f38
1245012809 <1245012809@qq.com>
2023-06-23 13:31:54 +0800

    bug修复--修复了UserMapper的问题，resultType写成了resultMap，导致一直识别不到

--- src/main/java/net/xdclass/online_xdclass/service/impl/UserServiceImpl.java
+++ src/main/java/net/xdclass/online_xdclass/service/impl/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.Map;
+import java.util.Random;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -40,7 +41,7 @@ public class UserServiceImpl implements UserService {
         if(userInfo.containsKey("phone") && userInfo.containsKey("pwd") && userInfo.containsKey("name")){
             User user = new User();
             user.setName(userInfo.get("name"));
-            user.setHeadImg("");
+            user.setHeadImg(getRandomImg());
             user.setCreateTime(new Date());
             user.setPhone(userInfo.get("phone"));
             String pwd = userInfo.get("pwd");
@@ -54,6 +55,26 @@ public class UserServiceImpl implements UserService {
 
     }
 
+    /**
+     * 放在CDN上的随即头像
+     */
+    private static final String [] headImg = {
+            "https://xd-video-pc-img.oss-cn-beijing.aliyuncs.com/xdclass_pro/default/head_img/12.jpeg",
+            "https://xd-video-pc-img.oss-cn-beijing.aliyuncs.com/xdclass_pro/default/head_img/11.jpeg",
+            "https://xd-video-pc-img.oss-cn-beijing.aliyuncs.com/xdclass_pro/default/head_img/13.jpeg",
+            "https://xd-video-pc-img.oss-cn-beijing.aliyuncs.com/xdclass_pro/default/head_img/14.jpeg",
+            "https://xd-video-pc-img.oss-cn-beijing.aliyuncs.com/xdclass_pro/default/head_img/15.jpeg"
+    };
+
+
+
+    private String getRandomImg(){
+        int size = headImg.length;
+        Random random = new Random();
+        int index = random.nextInt(size);
+
+        return headImg[index];
+    }
 
 //    User findByPhone(@Param("phone") String phone){
 //
