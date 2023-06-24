bfae771712aacd8812e0310d6b9e1c7987da81af
1245012809 <1245012809@qq.com>
2023-06-24 23:59:58 +0800

    功能新增--下单功能

--- src/main/java/net/xdclass/online_xdclass/service/impl/VideoOrderServiceImpl.java
+++ src/main/java/net/xdclass/online_xdclass/service/impl/VideoOrderServiceImpl.java
@@ -25,7 +25,7 @@ public class VideoOrderServiceImpl implements VideoOrderService {
 
     /**
      * 下单操作
-     * 未来版本：优惠券抵扣、风控用户检测、生成订单基础信息，生成支付信息
+     * 未来版本：优惠券抵扣、风控用户检测、生成订单基础信息，生成支付信息。
      *
      * @param userId
      * @param videoId
