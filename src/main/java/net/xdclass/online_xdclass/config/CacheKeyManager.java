42ee5e22471703272fa901c68dce34d380b9398a
1245012809 <1245012809@qq.com>
2023-06-29 09:54:33 +0800

    功能新增--把视频列表和轮播图列表都采用了Guava缓存

--- src/main/java/net/xdclass/online_xdclass/config/CacheKeyManager.java
+++ src/main/java/net/xdclass/online_xdclass/config/CacheKeyManager.java
@@ -1,5 +1,8 @@
 package net.xdclass.online_xdclass.config;
 
 public class CacheKeyManager {
-    public static final String INDEX_BANNER_KEY = "index:banner";
+    public static final String INDEX_BANNER_LIST_KEY = "index:banner:list";
+    public static final String INDEX_VIDEO_LIST_KEY = "index:video:list";
+
+    public static final String INDEX_DETAIL = "index:video:detail:%s";
 }
--- src/main/java/net/xdclass/online_xdclass/service/impl/VideoServiceImpl.java
+++ src/main/java/net/xdclass/online_xdclass/service/impl/VideoServiceImpl.java
@@ -1,5 +1,6 @@
 package net.xdclass.online_xdclass.service.impl;
 
+import javafx.geometry.VerticalDirection;
 import net.xdclass.online_xdclass.config.CacheKeyManager;
 import net.xdclass.online_xdclass.model.entity.Video;
 import net.xdclass.online_xdclass.model.entity.VideoBanner;
@@ -22,17 +23,42 @@ public class VideoServiceImpl implements VideoService {
 
     @Override
     public List<Video> listVideo() {
-        return videoMapper.listVideo();
+        try {
+            Object object = baseCache.getTenMinuteCache().get(CacheKeyManager.INDEX_VIDEO_LIST_KEY, () -> {
+                List<Video> videoList = videoMapper.listVideo();
+                System.out.println("\n=========Guava缓存开启==========\n");
+                return videoList;
+            });
+
+            if (object instanceof List){
+                List<Video> videoList = (List<Video>) object;
+                System.out.println("\n判断obejct是否存在东西了");
+                return videoList;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        System.out.println("\nImpl的listVedio出了问题\n");
+        return null;
     }
 
     @Override
     public List<VideoBanner> listBanner() {
         try {
-            Object object = baseCache.getTenMinuteCache().get(CacheKeyManager.INDEX_BANNER_KEY, () -> {
+            Object object = baseCache.getTenMinuteCache().get(CacheKeyManager.INDEX_BANNER_LIST_KEY, () -> {
                 List<VideoBanner> bannerList = videoMapper.listVideoBanner();
+                System.out.println("\n=============Guava缓存开启===============\n");
                 return bannerList;
             });
-            return videoMapper.listVideoBanner();
+
+            if (object instanceof List) {
+//            if (object != null) { 后边看看这么写会发生什么问题
+                List<VideoBanner> bannerList = (List<VideoBanner>) object;
+                return bannerList;
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -41,10 +67,28 @@ public class VideoServiceImpl implements VideoService {
 
     @Override
     public Video findDetailById(int videoId) {
-        //TODO 需要使用mybatis关联复杂查询
-        Video video = videoMapper.findDetailById(videoId);
 
-        return video;
+        try {
+            Object object = baseCache.getTenMinuteCache().get(CacheKeyManager.INDEX_BANNER_KEY, () -> {
+                Video video = videoMapper.findDetailById(videoId);
+                System.out.println("\nvideoId:"+videoId+"\n");
+                System.out.println("\n=============Guava缓存开启===============\n");
+                return video;
+            });
+
+            if (object instanceof Video) {
+                System.out.println("判断obejct是否存在东西了");
+                Video video = (Video) object;
+                return video;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+//        Video video = videoMapper.findDetailById(videoId);
+//
+//        return video;
 
     }
 }
